733c4a05cf4dcdd90fdb6b045f24c6c5057d8dac
mishan88 <mishanhideaki88@gmail.com>
2017-05-15 23:04:04 +0900

    change message to JSON

--- DCR/sacred/static/sacred/chat.js
+++ DCR/sacred/static/sacred/chat.js
@@ -1,17 +1,29 @@
 const socket = new WebSocket('ws://' + window.location.host + '/chat' + window.location.pathname.slice(12))
 
 socket.addEventListener('open', function(event){
-    socket.send('New friends coming' + 'in room' + window.location.pathname.slice(12));
+    socket.send(
+        JSON.stringify({
+            name:'ROOM',
+            message: 'New friends coming' + 'in room' + window.location.pathname.slice(12)
+        })
+    )
 });
 
 socket.addEventListener('message', function(event){
-    const sendingMessage = document.createElement('div')
-    sendingMessage.textContent = event.data
-    document.getElementById('messagelist').appendChild(sendingMessage)
+    if(JSON.parse(event.data).hasOwnProperty('message')){
+        const sendingMessage = document.createElement('div')
+        sendingMessage.textContent = JSON.parse(event.data).name + ':' + JSON.parse(event.data).message
+        document.getElementById('messagelist').appendChild(sendingMessage)
+    }
 });
 
 document.getElementById('inputmessage').addEventListener('submit', function(event){
     event.preventDefault();
-    socket.send(document.getElementById('id_your_name').value + ' : ' +document.getElementById('id_your_message').value);
+    socket.send(
+        JSON.stringify({
+            name: document.getElementById('id_your_name').value,
+            message: document.getElementById('id_your_message').value
+        })
+    );
     document.getElementById('id_your_message').value = ''
 })
