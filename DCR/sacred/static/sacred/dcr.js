7fa947dcb74720aa8b67a1e6ab3a26bafb2b4be7
mishan88 <mishanhideaki88@gmail.com>
2017-05-24 22:19:51 +0900

    get uploaed images

--- DCR/sacred/static/sacred/dcr.js
+++ DCR/sacred/static/sacred/dcr.js
@@ -1,6 +1,20 @@
 const modal = document.getElementById('imagemodal')
+const imagelist = document.getElementById('imagelist')
 document.getElementById('komabutton').addEventListener('click',function(event){
     modal.style.display = 'block';
+    fetch('http://' + window.location.host + '/api/images/').then(function(response){
+        if(response.ok){
+            response.json().then(function(myjson){
+                const imagec = document.createElement('img')
+                for(let i = 0; i < myjson.length; i++){
+                    imagec.src = myjson[i].image;
+                    imagelist.appendChild(imagec);
+                }
+            })
+        } else {
+            console.log('Network trouble?')
+        }
+    })
 })
 
 document.getElementById('closemodal').addEventListener('click',function(event){
