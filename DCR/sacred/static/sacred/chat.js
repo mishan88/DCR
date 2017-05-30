267e03b3892d6779fa2155e435f58a1a98477edd
mishan88 <mishanhideaki88@gmail.com>
2017-05-31 05:03:47 +0900

    auto scroll down

--- DCR/sacred/static/sacred/chat.js
+++ DCR/sacred/static/sacred/chat.js
@@ -1,4 +1,5 @@
 const socket = new WebSocket('ws://' + window.location.host + '/chat' + window.location.pathname.slice(12))
+const chatmessage = document.getElementById('chat-message')
 
 socket.addEventListener('open', function(event){
     socket.send(
@@ -11,11 +12,12 @@ socket.addEventListener('open', function(event){
 
 socket.addEventListener('message', function(event){
     if(JSON.parse(event.data).hasOwnProperty('message')){
-        const sendingMessage = document.createElement('div')
-        sendingMessage.className = 'output-message'
-        sendingMessage.textContent = JSON.parse(event.data).name + ':' + JSON.parse(event.data).message
-        document.getElementById('messagelist').appendChild(sendingMessage)
-    }
+        const sendingMessage = document.createElement('div');
+        sendingMessage.className = 'output-message';
+        sendingMessage.textContent = JSON.parse(event.data).name + ':' + JSON.parse(event.data).message;
+        document.getElementById('messagelist').appendChild(sendingMessage);
+        chatmessage.scrollTo(0,chatmessage.scrollHeight);
+    };
 });
 
 document.getElementById('inputmessage').addEventListener('submit', function(event){
@@ -27,4 +29,5 @@ document.getElementById('inputmessage').addEventListener('submit', function(even
         })
     );
     document.getElementById('id_your_message').value = ''
+    chatmessage.scrollTo(0,chatmessage.scrollHeight);
 })
