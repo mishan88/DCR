504d3250a5559a9c62754fd027fb5b6f83e03f2c
mishan88 <mishanhideaki88@gmail.com>
2017-06-04 17:02:41 +0900

    add URL Filter

--- DCR/DCR/settings.py
+++ DCR/DCR/settings.py
@@ -46,7 +46,10 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
        'rest_framework.permissions.AllowAny',
-    )
+    ),
+    'DEFAULT_FILTER_BACKENDS': [
+        'url_filter.integrations.drf.DjangoFilterBackend',
+    ]
 }
 
 MIDDLEWARE = [
--- DCR/images/views.py
+++ DCR/images/views.py
@@ -1,7 +1,12 @@
+"""Image API View"""
 from rest_framework import viewsets
 from .models import Images
 from .serializer import ImagesSerializer
 
+
 class ImageViewSet(viewsets.ModelViewSet):
+    """ImageView."""
+
     queryset = Images.objects.all()
     serializer_class = ImagesSerializer
+    filter_fields = ['room', 'imagetype']
