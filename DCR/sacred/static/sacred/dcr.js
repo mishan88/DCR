5f710a0c11f247cb2a4353f40103e990dd91b322
mishan88 <mishanhideaki88@gmail.com>
2017-06-04 20:04:53 +0900

    select KOMA

--- DCR/sacred/static/sacred/dcr.js
+++ DCR/sacred/static/sacred/dcr.js
@@ -1,17 +1,42 @@
 const modal = document.getElementById('imagemodal')
 document.getElementById('komabutton').addEventListener('click',function(event){
     modal.style.display = 'block';
-    fetch('http://' + window.location.host + '/api/images/').then(function(response){
+    fetch('//' + window.location.host + '/api/images/?room=' + window.location.pathname.slice(12)).then(function(response){
         if(response.ok){
-            response.json().then(function(myjson){
-                for(let i = 0; i < myjson.length; i++){
+            response.json().then(function(apiimage_json){
+                for(let numberOfImages = 0; numberOfImages < apiimage_json.length; numberOfImages++){
+                    const imageformfield = document.createElement('div');
+                    imageformfield.className = 'mdc-form-field';
+                    imageformfield.classList.add('image-form-field');
+                    const imageradio = document.createElement('div');
+                    imageradio.className = 'mdc-radio';
+                    imageradio.classList.add('image-radio');
                     const imageinput = document.createElement('input');
                     imageinput.type = 'radio';
+                    imageinput.className = 'mdc-radio__native-control';
+                    imageinput.id = apiimage_json[numberOfImages].id;
+                    imageinput.name = 'imageselect';
+
+                    const imageradio_background = document.createElement('div');
+                    imageradio_background.className = 'mdc-radio__background';
+                    imageradio_background.classList.add('imageradio_background');
+                    const imageradio_background_outercircle = document.createElement('div');
+                    imageradio_background_outercircle.className = 'mdc-radio__outer-circle';
+                    const imageradio_background_innercircle = document.createElement('div');
+                    imageradio_background_innercircle.className = 'mdc-radio__inner-circle';
+
                     const imagelabel = document.createElement('label');
                     imagelabel.className = 'selectimage-ch';
-                    imagelabel.style.backgroundImage = "url(" + myjson[i].image + ")"
-                    document.getElementById('imagelist').appendChild(imageinput);
-                    document.getElementById('imagelist').appendChild(imagelabel);
+                    imagelabel.style.backgroundImage = "url(" + apiimage_json[numberOfImages].image + ")"
+                    imagelabel.htmlFor = apiimage_json[numberOfImages].id;
+
+                    document.getElementById('imagelist').appendChild(imageformfield);
+                    document.getElementsByClassName('image-form-field')[numberOfImages].appendChild(imageradio);
+                    document.getElementsByClassName('image-radio')[numberOfImages].appendChild(imageinput);
+                    document.getElementsByClassName('image-radio')[numberOfImages].appendChild(imageradio_background);
+                    document.getElementsByClassName('imageradio_background')[numberOfImages].appendChild(imageradio_background_outercircle);
+                    document.getElementsByClassName('imageradio_background')[numberOfImages].appendChild(imageradio_background_innercircle);
+                    document.getElementsByClassName('image-form-field')[numberOfImages].appendChild(imagelabel);
                 };
             })
         } else {
