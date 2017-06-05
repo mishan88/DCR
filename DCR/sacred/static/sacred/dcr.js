19538eb06611d5aa4bbc760e692168ff200a6f46
mishan88 <mishanhideaki88@gmail.com>
2017-06-05 23:12:05 +0900

    add koma function

--- DCR/sacred/static/sacred/dcr.js
+++ DCR/sacred/static/sacred/dcr.js
@@ -80,21 +80,17 @@ document.getElementById('imageupload').addEventListener('click',function(event){
     postimagemodal.style.display = 'block';
 })
 
+// koma
 
 document.getElementById('putkoma').addEventListener('click',function(event){
-    const imageObj = new Image();
-    imageObj.src = document.querySelector('input[name="imageselect"]:checked').value;
-    const koma = new Konva.Image({
-        x:100,
-        y:100,
-        image: imageObj,
-        width:20,
-        height:20,
-        draggable:true
-    });
-    komalist.push(koma)
-    layer.add(koma);
-    stage.add(layer)
+    socket.send(
+        JSON.stringify({
+            _id: komalist.length + 3,
+            x: 20,
+            y: 20,
+            image_src : document.querySelector('input[name="imageselect"]:checked').value,
+        })
+    )
 });
 
 const stage = new Konva.Stage({
@@ -106,3 +102,75 @@ const layer = new Konva.Layer();
 stage.add(layer);
 
 const komalist = []
+
+//chat
+
+const socket = new WebSocket('ws://' + window.location.host + '/chat' + window.location.pathname.slice(12))
+const chatmessage = document.getElementById('chat-message')
+
+socket.addEventListener('open', function(event){
+    socket.send(
+        JSON.stringify({
+            name:'ROOM',
+            message: 'New friends coming' + 'in room' + window.location.pathname.slice(12)
+        })
+    )
+});
+
+socket.addEventListener('message', function(event){
+    if(JSON.parse(event.data).hasOwnProperty('message')){
+        const sendingMessage = document.createElement('div');
+        sendingMessage.className = 'output-message';
+        sendingMessage.textContent = JSON.parse(event.data).name + ':' + JSON.parse(event.data).message;
+        document.getElementById('messagelist').appendChild(sendingMessage);
+        chatmessage.scrollTo(0,chatmessage.scrollHeight);
+    };
+    if(JSON.parse(event.data).hasOwnProperty('x')){
+        console.log(JSON.parse(event.data));
+
+        if(Array.from({length:komalist.length}, (v,k) => k + 3).includes(JSON.parse(event.data)._id)){
+            komalist[JSON.parse(event.data)._id -3].setAttrs({
+                'x':JSON.parse(event.data).x,
+                'y':JSON.parse(event.data).y
+            })
+            //layer.add(koma);
+            stage.add(layer);
+        } else {
+            const imageObj = new Image();
+            imageObj.src = JSON.parse(event.data).image_src;
+            const koma = new Konva.Image({
+                x:JSON.parse(event.data).x,
+                y:JSON.parse(event.data).y,
+                image: imageObj,
+                width:20,
+                height:20,
+                draggable:true
+            });
+            komalist.push(koma);
+            layer.add(koma);
+            stage.add(layer);
+            komalist[komalist.length - 1].on('dragend',function(){
+                socket.send(
+                    JSON.stringify({
+                        _id: this._id,
+                        x: this.x(),
+                        y: this.y(),
+                        image_src : this.image().src,
+                    })
+                )
+            });
+        }
+    }
+});
+
+document.getElementById('inputmessage').addEventListener('submit', function(event){
+    event.preventDefault();
+    socket.send(
+        JSON.stringify({
+            name: document.getElementById('id_your_name').value,
+            message: document.getElementById('id_your_message').value
+        })
+    );
+    document.getElementById('id_your_message').value = ''
+    chatmessage.scrollTo(0,chatmessage.scrollHeight);
+})
